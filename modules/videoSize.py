95f85e799631a47cf27017929fb33d56d3e17b76
lernEarnBurn <lerners124@gmail.com>
2023-06-12 19:37:33 +0300

    func that returns an images dimensions

---
+++ modules/videoSize.py
@@ -0,0 +1,9 @@
+import cv2
+
+def get_video_dimensions(video_path):
+    vid = cv2.VideoCapture(video_path)
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+    
+    return width, height
+
