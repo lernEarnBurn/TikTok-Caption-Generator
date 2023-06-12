d05be254b8057691bdb2e4dcf6b74d0191c70352
lernEarnBurn <lerners124@gmail.com>
2023-06-12 23:57:29 +0300

    bug fix for a blank segment

--- modules/captions.py
+++ modules/captions.py
@@ -13,23 +13,27 @@ def transcribe_audio(path):
 
     video_filename = os.path.splitext(os.path.basename(path))[0]
 
+    
+
     for segment in segments:
-        start_time = timedelta(seconds=float(segment['start']))
-        end_time = timedelta(seconds=float(segment['end']))
+        if segment['text'] != '':
 
-        start_datetime = datetime(1, 1, 1) + start_time
-        end_datetime = datetime(1, 1, 1) + end_time
+            start_time = timedelta(seconds=float(segment['start']))
+            end_time = timedelta(seconds=float(segment['end']))
 
-        start_time_formatted = start_datetime.strftime("%H:%M:%S,%f")[:-3]
-        end_time_formatted = end_datetime.strftime("%H:%M:%S,%f")[:-3]
+            start_datetime = datetime(1, 1, 1) + start_time
+            end_datetime = datetime(1, 1, 1) + end_time
 
-        text = segment['text']
-        segment_id = segment['id'] + 1
-        segment_content = f"{segment_id}\n{start_time_formatted} --> {end_time_formatted}\n{text[1:] if text[0] == ' ' else text}\n\n"
+            start_time_formatted = start_datetime.strftime("%H:%M:%S,%f")[:-3]
+            end_time_formatted = end_datetime.strftime("%H:%M:%S,%f")[:-3]
 
-        srt_filename = os.path.join("./SrtFiles", f"{video_filename}.srt")
-        with open(srt_filename, 'a', encoding='utf-8') as srt_file:
-            srt_file.write(segment_content)
+            text = segment['text']
+            segment_id = segment['id'] + 1
+            segment_content = f"{segment_id}\n{start_time_formatted} --> {end_time_formatted}\n{text[1:] if text[0] == ' ' else text}\n\n"
+
+            srt_filename = os.path.join("./SrtFiles", f"{video_filename}.srt")
+            with open(srt_filename, 'a', encoding='utf-8') as srt_file:
+                srt_file.write(segment_content)
 
     print('\n\nSRT generated')
 
