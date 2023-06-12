093dbd2bd32a7ccad896ddcc1375a271469b56b9
lernEarnBurn <lerners124@gmail.com>
2023-06-12 20:18:47 +0300

    Working

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from captions import transcribe_audio
+from modules.captions import transcribe_audio
+from modules.writeText import add_caption_overlay
 import tkinter as tk
 from tkinter import filedialog
+import os
+
 
 
 def main():
     root = tk.Tk()
-    root.withdraw()  # Hide the root window
+    root.withdraw()  
 
-    # Open a file selection dialog
     file_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4")])
+    file_name = os.path.splitext(os.path.basename(file_path))[0]
+
     transcribe_audio(file_path)
+    add_caption_overlay(file_path, f'./srtFiles/{file_name}.srt')
 
 if __name__ == "__main__" :
     main()
+    #add_caption_overlay('./testVid.mp4', './srtFiles/testVid.srt')
+    #transcribe_audio('./testVid.mp4')
--- modules/writeText.py
+++ modules/writeText.py
@@ -2,10 +2,13 @@ from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 import os
 import pysrt
 from datetime import datetime as dt
+from .videoSize import get_video_dimensions
 
 os.environ["IMAGEIO_FFMPEG_EXE"] = r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\ffmpeg.exe"
 
 def add_caption_overlay(video_path, srt_path):
+    video_filename = os.path.splitext(os.path.basename(video_path))[0]
+
     video = VideoFileClip(video_path)
 
     subs = pysrt.open(srt_path)
@@ -22,7 +25,10 @@ def add_caption_overlay(video_path, srt_path):
 
         duration_seconds = (end_time - start_time).total_seconds()
 
-        text = TextClip(caption_text, fontsize=55, color='white', font='Corbel', method='caption', stroke_color='white', stroke_width=2.0)
+        screensize = get_video_dimensions(video_path)
+        
+
+        text = TextClip(caption_text, fontsize=55, color='white', font='Corbel', method='caption', stroke_color='white', stroke_width=2.5, size=screensize)
 
         text = text.set_position(('center')).set_start(start_time_float).set_duration(duration_seconds)
 
@@ -30,7 +36,6 @@ def add_caption_overlay(video_path, srt_path):
 
     final_clip = CompositeVideoClip([video] + clips)
     final_clip = final_clip.set_duration(video.duration)
-    final_clip.write_videofile('../output/testVid.mp4', codec='libx264', audio_codec='aac')
+    final_clip.write_videofile(f'./output/{video_filename}.mp4', codec='libx264', audio_codec='aac')
 
-if __name__ == "__main__":
-    add_caption_overlay('../testVid.mp4', '../srtFiles/testVid.srt')
+    
